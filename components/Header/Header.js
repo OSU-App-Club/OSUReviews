21a8bc29d6b2e70648964f96237af5b5d7e969e5
Dannon <gilbedan@oregonstate.edu>
2020-02-26 18:28:18 -0800

    Added next folder to .gitignore, added Header component, and imported into index page

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,4 +1,8 @@
 import './Header.css';
+import Link from 'next/link'
+import Head from 'next/head';
+
+import { useState } from 'react';
 
 const Header = (props) => {
     const {
@@ -6,17 +10,49 @@ const Header = (props) => {
         ...rest
     } = props;
 
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+    const Drawer = <div className="header__mobile__drawer">
+        <div>
+            <button
+                className={mobileMenuOpen ? "open" : "close"}
+                onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
+            >
+
+                &#10005;
+            </button>
+        </div>
+        <div className="header__mobile__drawer__first">
+            <p>About</p>
+        </div>
+        <div>
+            <p>Contact</p>
+        </div>
+    </div>;
+
     return (
-        <div
-            className={className}
-            {...rest}
-        >
+        <div className={className}>
             <div className="header__left">
-                <p>Home</p>
+                <Link href="/">
+                    <p>OSU<span className="black_text">Reviews</span></p>
+                </Link>
             </div>
             <div className="header__right">
                 <p>About</p>
                 <p>Contact</p>
+            </div>
+            <div className="header__right__mobile">
+                <button
+                    className={mobileMenuOpen ? "open" : "close"}
+                    onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
+                >
+                    <svg width="42" height="32" viewBox="0 0 42 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <line className="top_line" y1="2" x2="42" y2="2" stroke="black" stroke-width="4" />
+                        <line className="middle_line" y1="16" x2="42" y2="16" stroke="black" stroke-width="4" />
+                        <line className="bottom_line" y1="30" x2="42" y2="30" stroke="black" stroke-width="4" />
+                    </svg>
+                </button>
+                {mobileMenuOpen && Drawer}
             </div>
         </div>
     );
