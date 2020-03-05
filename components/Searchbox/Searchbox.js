bcaab1e5d7e5eacda215f6fcf90965e1fcd87cf0
Gilbert <gilbedan@tss.oregonstate.edu>
2020-03-05 12:29:39 -0800

    Added funcionality to search bar and removed next folder

--- components/Searchbox/Searchbox.js
+++ components/Searchbox/Searchbox.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../Searchbox/Searchbox.css'
 
 export default function index() {
+
+    const [dataShown, setDataShown] = useState([]);
+    const [searchValue, setSearchValue] = useState('');
+    const [selected, setSelected] = useState({});
 
     const data = [
         {
@@ -16,22 +20,48 @@ export default function index() {
             professor: 'Paris Kalathas',
             rating: '1.0'
         }
-    ]; 
+    ];
 
-  return (
-    <div>
-        <input className="searchbox" placeholder="Search classes or professors"/>
-        <div className="searchResultParent">
-            {data.map((data) => 
-            <div className="searchResult" key={data.id}>
-                <div className="searchResultColumn">
-                    <div className="searchResultProfessor">{data.professor}</div>
-                    <div className="searchResultName">{data.name}</div> 
-                </div>
-                <div className="searchResultRating">{data.rating}</div>
-            </div>
-            )}
+    const handleChange = ({ target: { value } }) => {
+        setSearchValue(value);
+        matchSearch();
+    }
+
+    const matchSearch = () => {
+        const emptyArray = [];
+        setDataShown(emptyArray);
+
+        data.map(item => {
+            if (item.professor.toLowerCase().includes(searchValue.toLowerCase()))
+                setDataShown(dataShown => [...dataShown, item]);
+        })
+    }
+
+    const handleSelect = (item) => {
+        setSelected(item);
+        setSearchValue('');
+        console.log(item);
+    }
+
+    return (
+        <div>
+            <input
+                className="searchbox"
+                placeholder="Search classes or professors"
+                value={searchValue}
+                onChange={(e) => handleChange(e)}
+            />
+            {searchValue.length > 0 &&
+                dataShown.map((data) =>
+                    <div className="searchResult" key={data.id} onClick={() => handleSelect(data)}>
+                        <div className="searchResultColumn">
+                            <div className="searchResultProfessor">{data.professor}</div>
+                            <div className="searchResultName">{data.name}</div>
+                        </div>
+                        <div className="searchResultRating">{data.rating}</div>
+                    </div>
+                )
+            }
         </div>
-    </div>
-  )
+    )
 }
