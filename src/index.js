b3b1d2e9ab6e847ae8896f4491fa2fa9156ef8cb
plim1025 <plim1025@gmail.com>
2020-04-01 18:54:53 -0700

    Added express files

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import App from './app.js';
-
-const element = document.getElementById('root');
-
-ReactDOM.render(<App />, element);
