d0608c50e898e714516e069cffaeb8a069acca93
Dannon Gilbert <gilbedan@oregonstate.edu>
2020-04-08 19:03:11 -0700

    Added mongoDB atlas connection, put .env file in gitignore, and added environment variables in server.js

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
 
-const port = 5000;
+const port = process.env.PORT || 5000;
+const mongoUser = process.env.USERNAME;
+const mongoPassword = process.env.PASSWORD;
+
+mongoose.connect(`mongodb+srv://${mongoUser}:${mongoPassword}@paprplanemongo-wy4yv.mongodb.net/test?retryWrites=true&w=majority`, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+});
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
